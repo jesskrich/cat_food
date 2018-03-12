d26963bf07f76b72ad900a5a934983f96d84cde4
Jessie Richardson <jessie.richardson04@gmail.com>
2018-03-12 15:48:27 -0400

    Initial commit

--- calculator.rb
+++ calculator.rb
@@ -5,5 +5,22 @@ class Calculator
   end
 
   def calculate
+    total = 0
+    @person.cats.each do |c|
+      case c.breed
+        when "tabby"
+          food = 10 - c.age
+        when "siamese"
+          food = 20 - (c.age * 2)
+        when "reverse_growth"
+          food = 1 + c.age
+      end
+      if food > 0
+        total = total + food
+      else
+        total = total + 1
+      end
+    end
+    total
   end
 end
--- cat.rb
+++ cat.rb
@@ -4,7 +4,19 @@ class Cat
   attr_accessor :age
 
   def initialize(breed:, age:)
-    @breed = breed
-    @age = age
+    @breed = validate_breed(breed)
+    @age = validate_age(age)
+  end
+
+  def validate_breed(breed)
+    if ['tabby', 'siamese', 'reverse_growth'].include?(breed.downcase)
+      breed.downcase
+    end
+  end
+
+  def validate_age(age)
+    if age.to_i >= 0
+      age
+    end
   end
 end
